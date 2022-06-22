38a7cf63a451d4648c7380457be1defd53cbb8ec
yousafsabir <yousafsabir430516@gmail.com>
2022-06-22 19:44:11 +0500

    store and item card added

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,23 @@
 import React, { FC } from "react";
+import { Col, Row } from "react-bootstrap";
+import Items from "../data/items.json";
+import ItemCard from "../components/ItemCard";
 
 const Store: FC = () => {
-    return <div>Store</div>;
+    return (
+        <>
+            <h1>Store</h1>
+            <Row xs={1} md={2} lg={3} className="g-3">
+                {Items.map((item) => {
+                    return (
+                        <Col key={item.id}>
+                            <ItemCard {...item} />
+                        </Col>
+                    );
+                })}
+            </Row>
+        </>
+    );
 };
 
 export default Store;
